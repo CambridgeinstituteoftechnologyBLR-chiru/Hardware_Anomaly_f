3d7fd17d66fd652140ead562d7b42770cea24b59
chiranth <chirantan.24ece@cambridge.edu.in>
2026-05-31 16:14:50 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,31 +1,35 @@
-`timescale 1ns / 1ps
+`timescale 1ns/1ps
 `default_nettype none
 
-module tb ();
-    initial begin
-        $dumpfile("tb.vcd");
-        $dumpvars(0, tb);
-        #1;
-    end
+module tb;
 
-    reg        clk;
-    reg        rst_n;
-    reg        ena;
+    reg clk;
+    reg rst_n;
+    reg ena;
+
     reg  [7:0] ui_in;
     reg  [7:0] uio_in;
+
     wire [7:0] uo_out;
     wire [7:0] uio_out;
     wire [7:0] uio_oe;
 
-    // Instantiate your consolidated chip design
-    tt_um_hardware_anomaly_detection user_project (
-        .ui_in   (ui_in),
-        .uo_out  (uo_out),
-        .uio_in  (uio_in),
-        .uio_out (uio_out),
-        .uio_oe  (uio_oe),
-        .ena     (ena),
-        .clk     (clk),
-        .rst_n   (rst_n)
+    tt_um_hardware_anomaly_detection dut (
+        .ui_in(ui_in),
+        .uo_out(uo_out),
+        .uio_in(uio_in),
+        .uio_out(uio_out),
+        .uio_oe(uio_oe),
+        .ena(ena),
+        .clk(clk),
+        .rst_n(rst_n)
     );
+
+    initial begin
+        $dumpfile("tb.vcd");
+        $dumpvars(0, tb);
+    end
+
 endmodule
+
+`default_nettype wire
